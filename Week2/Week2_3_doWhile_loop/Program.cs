e4946036bcb5b3a62d71aefed8b5fefed0b04f92
Nazif Karaca <nazif808@gmail.com>
2024-10-02 14:08:36 +0300

    week2 do while exercise

---
+++ Week2/Week2_3_doWhile_loop/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Week2_3_doWhile_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+            string multiLine = new string('-', 35);
+            int limit;
+
+            // Get and validate input from the user for the while loop
+            do
+            {
+                Console.Write("Bir pozitif tamsayı limit değer giriniz: ");
+            } while (!int.TryParse(Console.ReadLine(), out limit) || limit <= 0);
+
+            // While loop
+            while (sayac < limit)
+            {
+                Console.WriteLine("Ben bir Patika'lıyım");
+                sayac++;
+            }
+
+            Console.WriteLine(multiLine);
+
+            int limit2 = 0;
+            // Do/while loop
+            do
+            {
+                // Reset sayac
+                sayac = 0;
+
+                // Get and validate input from the user for the do/while loop
+                do
+                {
+                    Console.Write("Bir pozitif tamsayı limit değer giriniz (çıkış için 0 yazın): ");
+                } while (!int.TryParse(Console.ReadLine(), out limit2));
+
+                while (sayac < limit2)
+                {
+                    Console.WriteLine("Ben bir Patika'lıyım");
+                    sayac++;
+                }
+
+            } while (limit2 != 0);
+        }
+    }
+}
