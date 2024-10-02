174b28a39d96dd8dba68599d26fc7f0aa0e0e4a1
Nazif Karaca <nazif808@gmail.com>
2024-10-02 14:29:00 +0300

    new project started

---
+++ Week2/Week2_4_TatilUygulaması/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Week2_4_TatilUygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                // todo: gitmek istediği lokasyon, veri olarak tut
+                // todo: büyük küçük duyarlılık, farklı lokasyonda hata, lokasyon isimleri hatırlat, yeni veri
+                // todo: kaç kişi olacak (veri tut)
+                // todo: lokasyon hakkında yapılabilecek bilgiler
+                // todo: hangi ulaşım şekli kullanılacak (hava yolu, kara yolu), değişkende tut, 1,2 olarak alınsın ve yanlış girildiyse hata versin
+                // todo: lokasyon + kişi sayısı + ulaşım aracı --> hesapla ve kullanıcıya sun
+                // todo: devam etmek istiyorsa baştan başla, etmek istemiyorsa sonlandır
+            } while (true);
+        }
+    }
+}
