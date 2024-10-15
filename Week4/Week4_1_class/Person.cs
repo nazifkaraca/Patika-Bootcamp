5f00dd862cae2d6c2c90e3ec14d0603e8bdcf583
Nazif Karaca <113899211+nazifkaraca@users.noreply.github.com>
2024-10-16 00:23:19 +0300

    Update Person.cs

--- Week4/Week4_1_class/Person.cs
+++ Week4/Week4_1_class/Person.cs
@@ -3,15 +3,13 @@
 	public class Person
 	{
 		public string Name { get; set; }
-        public string Surname { get; set; }
+        	public string Surname { get; set; }
 		public string Occupation { get; set; }
-
-        public DateTime BirthDate { get; set; }
-
+        	public DateTime BirthDate { get; set; }
 
 		public void DisplayPerson()
 		{
-            Console.WriteLine($"{Name} {Surname} isimli kişi bir {Occupation}'dir. {BirthDate} tarihinde doğmuştur.");
+            		Console.WriteLine($"{Name} {Surname} isimli kişi bir {Occupation}'dir. {BirthDate} tarihinde doğmuştur.");
 		}
 	}
 }
