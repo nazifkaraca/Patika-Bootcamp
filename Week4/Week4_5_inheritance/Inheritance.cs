50bf89c86e873c02bcaafeabe932fba645c575c0
Nazif Karaca <nazif808@gmail.com>
2024-10-16 22:19:36 +0300

    Week 4-5: Inheritance initial push.

---
+++ Week4/Week4_5_inheritance/Inheritance.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Threading.Channels;
+
+namespace Inheritance
+{
+	// Create base class inheriting a method and properties to child classes
+	public abstract class BasePerson
+	{
+		public required string Name { get; set; }
+		public required string Surname { get; set; }
+
+		// Virtual class to be overridden by child classes
+		public virtual void Introduce()
+		{
+            Console.WriteLine($"Merhaba {Name} {Surname}!");
+		}
+	}
+
+	// Sealed class to prevent inheritation
+	public sealed class Teacher : BasePerson
+    {
+		public int Salary { get; set; }
+
+		// Override the base class method
+        public override void Introduce()
+        {
+            base.Introduce();
+			Console.WriteLine($"Your salary is {Salary}.");
+        }
+    }
+
+    // Sealed class to prevent inheritation
+    public sealed class Student : BasePerson 
+	{ 
+		public int StudentNumber { get; set; }
+
+		// Override the base class method
+        public override void Introduce()
+        {
+            base.Introduce();
+            Console.WriteLine($"Your student number is {StudentNumber}.");
+        }
+    }
+}
---
+++ Week4/Week4_5_inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using Inheritance;
+
+namespace Week4_5_inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new instance of Teacher using an object initializer
+            Teacher teacher = new Teacher
+            {
+                Name = "Mustafa Kemal",
+                Surname = "Atatürk",
+                Salary = 1923
+            };
+            // Display the properties of the created instance
+            teacher.Introduce();
+
+            // Create multiple lines in the screen
+            Console.WriteLine(new string('-', 35));
+
+            // Create a new instance of Student using an object initializer
+            Student student = new Student
+            {
+                Name = "Sabiha",
+                Surname = "Gökçen",
+                StudentNumber = 1936
+            };
+            // Display the properties of the created instance
+            student.Introduce();
+
+            Console.ReadKey();
+        }
+    }
+}
