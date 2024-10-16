8152606368b9f4c2eeb53bcf6c3db9d33e051322
nazifkaraca <nazif808@gmail.com>
2024-10-16 14:09:07 +0300

    week 4 constructor added

---
+++ Week4/Week4_2_constructor/Bebek.cs
@@ -0,0 +1,29 @@
+﻿namespace Baby
+{
+	public class Bebek
+	{
+		// Creating the field and get/set operations
+		private string _name { get; set; }
+		private string _surname { get; set; }
+		private DateTime _birth { get; set; }
+
+		// Create an empty class which takes no parameters
+		public Bebek() 
+		{
+			_birth = DateTime.Now;
+
+			Console.WriteLine($"Ingaa\nİsim: İsimsiz\nSoyisim: Soyisimsiz\nDoğum Tarihi: {_birth}");
+		}
+
+		// Create a class which takes name and surname as parameters
+		public Bebek(string name,  string surname)
+		{
+			_name = name;
+			_surname = surname;
+			_birth = DateTime.Now;
+
+            Console.WriteLine($"Ingaa\nİsim: {name}\nSoyisim {surname}\nDoğum Tarihi: {_birth}");
+		}
+	}
+
+}
---
+++ Week4/Week4_2_constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using Baby;
+using System.Threading;
+
+namespace Week4_2_constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance from an no paramater class
+            Bebek bebek = new Bebek();
+            // Wait a sec to see if datetime works correctly
+            Thread.Sleep(1000);
+            // Create an instance from a constructor which takes parameters
+            Bebek bebekcan = new Bebek("bebek", "bebekcan");
+
+            Console.ReadKey();  
+        }
+    }
+}
