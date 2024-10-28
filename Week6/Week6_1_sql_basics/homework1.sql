0173667c846067e7d2afce7e1e3040a49f9e6b49
Nazif Karaca <113899211+nazifkaraca@users.noreply.github.com>
2024-10-28 18:57:26 +0300

    Update homework1.sql

--- Week6/Week6_1_sql_basics/homework1.sql
+++ Week6/Week6_1_sql_basics/homework1.sql
@@ -1,11 +1,15 @@
---SELECT title, description FROM film;
+--Q1
+SELECT title, description FROM film;
 
---SELECT * from film
---WHERE length > 60 AND length < 75;
+-- Q2
+SELECT * from film
+WHERE length > 60 AND length < 75;
 
---SELECT * FROM film
---WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR rental_rate = 0.99 AND replacement_cost = 28.99;
+-- Q3
+SELECT * FROM film
+WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR rental_rate = 0.99 AND replacement_cost = 28.99;
 
+-- Q4
 SELECT * from film
 WHERE NOT (length > 50 OR rental_rate = 2.99 OR rental_rate = 4.99);
 
