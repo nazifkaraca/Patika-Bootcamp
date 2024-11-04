965747382ecd528787e24653109e0a2be9c960ed
nazifkaraca <nazif808@gmail.com>
2024-11-04 16:38:31 +0300

    Week 7-1: LINQ exercises initial push. This week mainly focuses on LINQ and Lambda expressions.

---
+++ Week7/Week7_1_linq/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Week7_1_linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            List<int> list = new List<int>();
+            
+            // Generate 10 random number and assign them to list
+            for (int i = 0; i < 10; i++) list.Add(random.Next(-100, 101));
+
+            #region EvenNumbers
+            // Q1 Even numbers
+            Console.WriteLine("Çift sayılar:");
+
+            // Get only even numbers from list and assign them to evenNumbers
+            var evenNumbers = list.Where(num => num % 2 == 0);
+            // Loop through 
+            foreach (int num in evenNumbers) Console.WriteLine(num); 
+            #endregion
+
+            Console.WriteLine("--------------");
+
+            #region OddNumbers
+            // Q2 Odd numbers
+            Console.WriteLine("Tek sayılar:");
+
+            var oddNumbers = list.Where(num => num % 2 != 0);
+
+            foreach (var num in oddNumbers) Console.WriteLine(num); 
+            #endregion
+
+            Console.WriteLine("--------------");
+
+            #region NegativeNumbers
+            // Q3 Negative numbers
+            Console.WriteLine("Negatif sayılar:");
+
+            var negativeNumbers = list.Where(num => int.IsNegative(num));
+
+            foreach (var num in negativeNumbers) Console.WriteLine(num);
+            #endregion
+
+            Console.WriteLine("--------------");
+
+            #region PositiveNumbers
+            Console.WriteLine("Pozitif sayılar:");
+
+            var positiveNumbers = list.Where(num => int.IsPositive(num));
+
+            foreach (int num in positiveNumbers) Console.WriteLine(num);
+            #endregion
+
+            Console.WriteLine("--------------");
+
+            #region GreaterSmaller
+            Console.WriteLine("15'ten büyük 22'den küçük sayılar:");
+
+            var greaterSmaller = list.Where(num => num > 15 && num < 22);
+
+            foreach (int num in greaterSmaller) Console.WriteLine(num);
+
+            #endregion
+
+            Console.WriteLine("--------------");
+
+            #region Power
+            Console.WriteLine("Her sayının karesi: ");
+            // Take 2 to the power of a number
+            list.ForEach(num => Console.WriteLine($"{num} --> " + Math.Pow(num, 2)));
+
+            #endregion
+        }
+    }
+}
---
+++ Week7/Week7_1_linq/readme.md
@@ -0,0 +1,56 @@
+﻿# Pratik - LINQ Project
+
+This project aims to create a list of 10 random numbers and execute various LINQ queries on this list to display the filtered results on the console. The program is written in C# and leverages LINQ (Language Integrated Query) to efficiently query data from the list.
+
+## About the Project
+
+In this project, a list of 10 random numbers will be generated, and specific queries will be executed on the list using LINQ. Each query filters the list based on different criteria, displaying the results on the console. LINQ makes it easier, more readable, and efficient to perform filtering on lists.
+
+### LINQ Queries to be Performed
+
+The following LINQ queries will be implemented:
+
+- **Even Numbers**: Display even numbers in the list.
+- **Odd Numbers**: Display odd numbers in the list.
+- **Negative Numbers**: Display negative numbers in the list.
+- **Positive Numbers**: Display positive numbers in the list.
+- **Numbers Between 15 and 22**: Display numbers greater than 15 and less than 22.
+- **Square of Each Number**: Create a new list containing the squares of each number in the original list and display it on the console.
+
+## Technologies Used
+
+- **C#**: The programming language used for the project.
+- **LINQ (Language Integrated Query)**: Used for querying data on the list.
+
+## Requirements
+
+To run this project, you will need:
+
+- .NET Core SDK or .NET Framework
+- Visual Studio or a similar C# development environment
+
+## Usage
+
+To run the project, use Visual Studio or the command line. When executed, the program will automatically create a random list of numbers and display the results according to the specified criteria.
+
+Sample outputs might include:
+- Even Numbers: `4, 8, 20`
+- Odd Numbers: `3, 9, 15`
+- Negative Numbers: `-5, -9`
+- Positive Numbers: `3, 15, 20`
+- Numbers Between 15 and 22: `16, 21`
+- Squares of Numbers: `9, 25, 144`
+
+## Contributing
+
+If you would like to contribute to this project:
+
+1. Fork the project.
+2. Create a new branch: `git checkout -b feature-name`.
+3. Commit your changes: `git commit -m 'Add new feature'`.
+4. Push to the branch: `git push origin feature-name`.
+5. Open a Pull Request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
