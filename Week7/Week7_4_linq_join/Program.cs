0f4b76dce76613a3c3b16018a5cc93d4be2c383e
nazifkaraca <nazif808@gmail.com>
2024-11-06 14:58:58 +0300

    Week 7-4: LINQ Advanced topic of Join and Group Join exercise push.

---
+++ Week7/Week7_4_linq_join/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Week7_4_linq_join
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Authors> authors = new()
+            {
+                new Authors(1, "İhsan Oktay Anar"),
+                new Authors(2, "Sabahattin Ali"),
+                new Authors(3, "Montaigne"),
+                new Authors(4, "Ayn Rand")
+            };
+
+            List<Books> books = new()
+            {
+                new Books(1, "Amat", 1),
+                new Books(2, "Atlas Silkindi", 4),
+                new Books(3, "Hayatın Kaynağı", 4),
+                new Books(4, "Kürk Mantolu Madonna", 2),
+                new Books(5, "Denemeler", 3),
+                new Books(6, "İçimizdeki Şeytan", 2)
+            };
+
+            var booksNauthors = from author in authors
+                                join book in books on author.AuthorId equals book.AuthorId
+                                select new
+                                {
+                                    AuthorName = author.Name,
+                                    BookTitle = book.Title
+                                };
+
+            Console.WriteLine($"{"Yazar",-20} | {"Kitap",-15}");
+            foreach (var book in booksNauthors) Console.WriteLine($"{book.AuthorName,-20} | {book.BookTitle,-15}");
+
+            // İkinci Yol - Gruplayarak
+
+            var booksNauthors2 = authors.GroupJoin(books,
+                                                 author => author.AuthorId,
+                                                 book => book.AuthorId,
+                                                 (authors, books) => new
+                                                 {
+                                                     AuthorName = authors.Name,
+                                                     BookName = books,
+                                                 });   
+
+            foreach (var author in booksNauthors2)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{author.AuthorName}:");
+                Console.WriteLine("-------------------");
+
+                foreach (var book in author.BookName)
+                {
+                    Console.WriteLine($"Kitap: {book.Title}");
+                }
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Week7/Week7_4_linq_join/bookstore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7_4_linq_join
+{
+    public class Authors
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+
+        public Authors(int  authorId, string name)
+        {
+            AuthorId = authorId;
+            Name = name;
+        }
+    }
+
+    public class Books
+    {
+        public int BookId{ get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+
+        public Books(int bookId, string title, int authorId)
+        {
+            BookId = bookId;
+            Title = title;
+            AuthorId = authorId;
+        }
+    }
+}
