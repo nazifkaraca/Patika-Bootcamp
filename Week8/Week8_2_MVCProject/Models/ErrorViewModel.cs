b7f394b47d8305d4a0e175b30e1667df1fed5c9e
Nazif Karaca <nazif808@gmail.com>
2024-11-12 02:38:53 +0300

    Week 8-2: Unnecessary files deleted.

--- Week8/Week8_2_MVCProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Week8_2_MVCProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
