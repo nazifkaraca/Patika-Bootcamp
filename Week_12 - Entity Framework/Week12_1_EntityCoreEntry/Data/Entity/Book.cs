600db6184757bcc7673629d58685b2dde0e5975b
Nazif Karaca <nazif808@gmail.com>
2024-12-09 01:19:42 +0300

    Week 12-1.01: Entity Framework Core beginner exercise. Entities created with Data Annotations.

---
+++ Week_12 - Entity Framework/Week12_1_EntityCoreEntry/Data/Entity/Book.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Week12_1_EntityCoreEntry.Data.Entity
+{
+    [Table("Books")] // Determining table name
+    [Index(nameof(Author))] // Boosts search and filter by the author column
+    public class Book
+    {
+        [Key] // Indication key value
+        public int Id { get; set; }
+
+        [Required] // Indicating this property required
+        [StringLength(100)] // Maximum character limit
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+
+        [Column("BookPrice", TypeName = "decimal(18, 2)")] // Set custom column name and type (2 floating points)
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+
+        [Column("LastBorrowed")] // Set custom name for a column
+        public DateTime? LastBarrowedDate { get; set; }
+
+        // Navigation Reference Property
+        public List<Review> Reviews { get; set; }
+    }
+}
---
+++ Week_12 - Entity Framework/Week12_1_EntityCoreEntry/Data/Entity/Review.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Week12_1_EntityCoreEntry.Data.Entity
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public string ReviewerName { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+
+        [ForeignKey(nameof(BookId))] // Indicating book id as a foreign key
+        public Book Book { get; set; }
+    }
+}
