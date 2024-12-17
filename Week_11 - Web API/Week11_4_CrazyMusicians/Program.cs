e81fe3f9d2769b03489af43df5622cc3ad2c68ea
nazifkaraca <nazif808@gmail.com>
2024-12-17 11:11:09 +0300

    Week 13-1: Dependency Injection practice initial push.

--- Week_11 - Web API/Week11_4_CrazyMusicians/Program.cs
+++ Week_11 - Web API/Week11_4_CrazyMusicians/Program.cs
@@ -18,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ Week_13 - Dependency Injection/Week13_1_Classroom/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Week13_1_Classroom
+{ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher
+            {
+                FirstName = "Mustafa",
+                LastName = "Kemal"
+            };
+
+            ClassRoom classRoom = new ClassRoom(teacher);
+
+            classRoom.GetTeacherInfo();
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface ITeacher
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string GetInfo();
+    }
+
+    public class Teacher : ITeacher
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public string GetInfo() 
+        {
+            return $"Öğretmen: {FirstName} {LastName}";
+        }
+    }
+
+    public class ClassRoom
+    {
+        public readonly Teacher _teacher;
+
+        public ClassRoom(Teacher teacher)
+        {
+            _teacher = teacher;
+        }
+
+        public void GetTeacherInfo()
+        {
+            Console.WriteLine(_teacher.GetInfo());
+        }
+    }
+}
+
+
+
